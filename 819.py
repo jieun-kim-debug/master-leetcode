f043a73f7c431f9deb726976dd7b1bfd4537824a
jieun-kim-debug <onelife90@naver.com>
2020-08-29 15:22:32 +0900

    re

--- 819.py
+++ 819.py
@@ -1,9 +1,15 @@
+'''
+Input: 
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+Output: "ball"
+'''
+#1. paragraph 안 특수문자 제거, 소문자 처리
+#2. counter()로 단어 세기
+#3. 가장 많이 나온 단어의 인덱스 출력
+
+import collections
+
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        #1. split로 나누기
-        #2. 리스트 내 중복요소 찾기
-        #3. 가장 많이 나온 중복요소 print
-        a = []
-        a.append(paragraph.split())
-        # print(a)
         
