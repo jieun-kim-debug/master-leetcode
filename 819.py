51e60ebc8b981a2385b051a006de60eae40c93ff
jieun-kim-debug <onelife90@naver.com>
2020-08-29 15:39:42 +0900

    counter key value

--- 819.py
+++ 819.py
@@ -9,7 +9,17 @@ Output: "ball"
 #3. 가장 많이 나온 단어의 인덱스 출력
 
 import collections
+import re
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        
+        # re.sub('패턴', '바꿀문자열', '문자열', 바꿀횟수)
+        words = [word for word in re.sub('[^\w]', ' ', paragraph.lower().split())
+                 if word not in banned]
+        # 전처리된 단어를 counter
+        count = collections.Counter(words)
+        # print(count)
+        # ({'ball': 2, 'bob': 1, 'a': 1, 'the': 1, 'flew': 1, 'far': 1, 'after': 1, 'it': 1, 'was': 1})
+
+        # 가장 많은 단어 1위를 가져오는데, key와 value라 [0][0]을 해야만 key값이 출력
+        return count.most_common(1)[0][0]
